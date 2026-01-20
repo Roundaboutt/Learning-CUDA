57ff48508d2a8ad6d8823ff891be53728c66519d
Rountaboutt <a109763648@qq.com>
2026-01-20 15:31:59 +0800

    完成trace_kernel

--- src/kernels.cu
+++ src/kernels.cu
@@ -17,12 +17,107 @@
  * @param cols Number of columns in the matrix.
  * @return The trace (sum of diagonal values) of the matrix.
  */
+#define BLOCKSIZE 1024
+#define WARPSIZE 32
+#define FULLMASK 0xffffffff
+
 template <typename T>
-T trace(const std::vector<T>& h_input, size_t rows, size_t cols) {
-  // TODO: Implement the trace function
-  return T(-1);
+__device__  T BlockReduce(T val)
+{
+    const int tid = threadIdx.x;
+    const int warpID = tid / WARPSIZE;
+    const int laneID = tid % WARPSIZE;
+
+    for (size_t offset = WARPSIZE / 2; offset > 0; offset >>= 1)
+    {
+        val += __shfl_down_sync(FULLMASK, val, offset);
+    }
+
+    __shared__ T warp_shared[BLOCKSIZE / WARPSIZE];
+    if (laneID == 0)
+    {
+        warp_shared[warpID] = val;
+    }
+    __syncthreads();
+
+    if (warpID == 0)
+    {
+        val = warp_shared[laneID];
+        for (size_t offset = WARPSIZE / 2; offset > 0; offset >>= 1)
+        {
+            val += __shfl_down_sync(FULLMASK, val, offset);
+        }
+    }
+    return val;
 }
 
+
+template <typename T>
+__global__ void trace_kernel(T* d_input, T* d_output, const size_t N)
+{
+    const size_t tid = threadIdx.x;
+    const size_t bid = blockIdx.x;
+    const size_t global_id = tid + bid * blockDim.x;
+
+    T sum = 0.f;
+    for (size_t i =  global_id; i < N; i += blockDim.x * gridDim.x)
+    {
+        sum += d_input[i];
+    }
+
+    sum = BlockReduce(sum);
+
+    if (tid == 0)
+        d_output[bid] = sum;
+
+}
+
+
+template <typename T>
+T trace(const std::vector<T>& h_input, size_t rows, size_t cols) {
+
+    if (rows > cols) rows = cols;
+    else cols = rows;
+
+    std::vector<T> temp;
+    for (int i = 0; i < rows; i++)
+    {
+        temp.push_back(h_input[i * cols + i]);
+    }
+
+    const size_t num_bytes = sizeof(T) * temp.size();
+    const int N = temp.size();
+
+    T* d_intput,* d_output;
+    cudaMalloc((void**)&d_intput, num_bytes);
+    cudaMalloc((void**)&d_output, num_bytes);
+    cudaMemcpy(d_intput, temp.data(), num_bytes, cudaMemcpyHostToDevice);
+    
+    size_t threads = BLOCKSIZE;
+    size_t n = N;
+    size_t blocks = (n + threads - 1) / threads;
+    T* d_in = d_intput;
+    T* d_out = d_output;
+
+    while (blocks > 1)
+    {
+        trace_kernel<T><<<blocks, threads>>>(d_in, d_out, n);
+        cudaDeviceSynchronize();
+
+        n = blocks;
+        blocks = (n + threads - 1) / threads;
+        std::swap(d_in, d_out);
+    }
+    trace_kernel<T><<<1, threads>>>(d_in, d_out, n);
+    T res = 0;
+    cudaMemcpy(&res, d_out, sizeof(T), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_output);
+    cudaFree(d_intput);
+
+
+    return res;
+}
 /**
  * @brief Computes flash attention for given query, key, and value tensors.
  * 
