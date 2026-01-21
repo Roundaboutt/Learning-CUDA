c77f7e15ec7b89504ec4153c450912a049d02892
Rountaboutt <a109763648@qq.com>
2026-01-21 21:26:09 +0800

    使用grid.sync()优化reduce

--- src/kernels.cu
+++ src/kernels.cu
@@ -1,5 +1,7 @@
 #include <vector>
 #include <cuda_fp16.h>
+#include <cooperative_groups.h>
+
 
 #include "../tester/utils.h"
 
@@ -23,6 +25,8 @@
 template <typename T>
 __device__ T WarpReduce(T val)
 {
+   
+#pragma unroll    
     for (int offset = warpSize / 2; offset > 0; offset >>= 1)
     {
         val += __shfl_down_sync(FULLMASK, val, offset);
@@ -31,39 +35,66 @@ __device__ T WarpReduce(T val)
 }
 
 
+namespace cg = cooperative_groups;
+
 template <typename T>
-__global__ void trace_kernel(const T* d_input, T* d_output, const size_t N)
+__global__ void reduce_kernel(const T* d_input, T* d_output, T* d_workspace, const size_t N)
 {
-
-    extern __shared__ char shared_mem[];
-    T* smem = reinterpret_cast<T*>(shared_mem);
-
+    cg::grid_group grid = cg::this_grid();
+    T sum = (T)0;
+    
     const size_t tid = threadIdx.x;
-    const size_t idx = tid + blockIdx.x * blockDim.x;
+    const size_t bid = blockIdx.x;
+    const size_t idx = tid + bid * blockDim.x;
+    const size_t laneID = tid % warpSize;
+    const size_t warpID = tid / warpSize;
 
-    T sum = 0;
-    for (int i = idx; i < N; i += blockDim.x * gridDim.x)
+
+    for (size_t i = idx; i < N; i += gridDim.x * blockDim.x)
     {
         sum += d_input[i];
     }
 
-    T warp_sum = WarpReduce<T>(sum);
+    // warp 内的和
+    T warp_sum = WarpReduce(sum);
 
-    const size_t warpID = tid / warpSize;
-    const size_t laneID = tid % warpSize;
+    __shared__ T smem[32];
+    if (laneID == 0) smem[warpID] = warp_sum;
+    __syncthreads();
 
-    if (laneID == 0)
+    // 用一个warp 对整个block内的所有warp之和归约
+    if (warpID == 0)
     {
-        smem[warpID] = warp_sum;
+        T block_sum = (tid < ((blockDim.x + warpSize - 1) / warpSize)) ? smem[laneID] : 0;
+        block_sum = WarpReduce(block_sum);
+        if(tid == 0) smem[0] = block_sum;
     }
     __syncthreads();
 
-    if (warpID == 0)
+    if (tid == 0) d_workspace[bid] = smem[0];
+    grid.sync();
+
+    // 用一个block对d_workspace中所有元素归约
+    if (bid == 0)
     {
-        T block_sum = (tid < (blockDim.x + warpSize - 1) / warpSize) ? smem[laneID] : 0;
-        block_sum = WarpReduce(block_sum);
-        if (tid == 0) atomicAdd(d_output, block_sum);
+        T final_sum = 0;
+        for (size_t i = idx; i < gridDim.x; i += blockDim.x)
+        {
+            final_sum += d_workspace[i];
+        }
+        final_sum = WarpReduce(final_sum);
+
+        if (laneID == 0) smem[warpID] = final_sum;
+        __syncthreads();
+
+        if (warpID == 0)
+        {
+            final_sum = (tid < ((blockDim.x + warpSize - 1) / warpSize)) ? smem[laneID] : 0;
+            final_sum = WarpReduce(final_sum);
+            if (tid == 0) *d_output = final_sum;
+        }
     }
+
 }
 
 template <typename T>
@@ -72,38 +103,49 @@ T trace(const std::vector<T>& h_input, size_t rows, size_t cols) {
     const size_t n_diag = (rows < cols) ? rows : cols;
     std::vector<T> temp;
     temp.reserve(n_diag);
+
+#pragma unroll    
     for (size_t i = 0; i < n_diag; i++) {
         temp.push_back(h_input[(size_t)i * cols + i]);
     }
 
 
-    const size_t N = temp.size();
-    const size_t num_bytes = sizeof(T) * N;
+    size_t N = temp.size();
 
-    T *d_input, *d_output;
-    cudaMalloc((void**)&d_input, num_bytes);
-    cudaMalloc((void**)&d_output, sizeof(T));
+    T *d_input, *d_output, *d_workspace;
+    cudaMalloc(&d_input, sizeof(T) * N);
+    cudaMalloc(&d_output, sizeof(T));
+    cudaMemcpy(d_input, temp.data(), sizeof(T) * N, cudaMemcpyHostToDevice);
     cudaMemset(d_output, 0, sizeof(T));
 
-    cudaMemcpy(d_input, temp.data(), num_bytes, cudaMemcpyHostToDevice);
+    int dev = 0;
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, dev);
 
-    int threadsPerBlock = 1024;
-    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+    int threadsPerBlock = prop.maxThreadsPerBlock;
+    size_t smem_size = (threadsPerBlock / 32) * sizeof(T); 
 
-    size_t smem_size = threadsPerBlock * sizeof(T);
+    int numBlocksPerSm = 0;
+    auto kernel_func = reduce_kernel<T>;
 
-    trace_kernel<T><<<blocksPerGrid, threadsPerBlock, smem_size>>>(d_input, d_output, N);
+    cudaOccupancyMaxActiveBlocksPerMultiprocessor(&numBlocksPerSm, kernel_func, threadsPerBlock, smem_size);
 
-    T h_res = 0;
-    cudaDeviceSynchronize();
+    int maxActiveBlocks = numBlocksPerSm * prop.multiProcessorCount;
+    int blocks = (N + threadsPerBlock - 1) / threadsPerBlock;
+    if (blocks > maxActiveBlocks) blocks = maxActiveBlocks;
 
-    cudaMemcpy(&h_res, d_output, sizeof(T), cudaMemcpyDeviceToHost);
+    cudaMalloc(&d_workspace, blocks * sizeof(T));
 
+    // 参数包
+    void* kernelArgs[] = { &d_input, &d_output, &d_workspace, &N };
 
-    cudaFree(d_input);
-    cudaFree(d_output);
+    cudaLaunchCooperativeKernel((void*)kernel_func, dim3(blocks), dim3(threadsPerBlock), kernelArgs, smem_size, 0);
 
-    return h_res;
+    T res;
+    cudaMemcpy(&res, d_output, sizeof(T), cudaMemcpyDeviceToHost);
+    
+    cudaFree(d_input); cudaFree(d_output); cudaFree(d_workspace);
+    return res;
 }
 /**
  * @brief Computes flash attention for given query, key, and value tensors.
